1e5e3cfe6f651ef37aac15170144117afd8e68f2
lucaschalela990 <lukeesf2016@gmail.com>
2021-11-30 10:45:51 -0300

    init repo

---
+++ src/test/java/com/lchalela/people/PeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lchalela.people;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
